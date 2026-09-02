f34937e9b19d8a0fc669b1388f415d3da1c7c61f
anddreaparra-ctrl <anddreaparra@alumnos.cei.es>
2026-09-02 09:02:51 +0200

    asd

---
+++ Clase 18/js/index.js
@@ -0,0 +1,12 @@
+let edad
+edad = 7
+
+let animal
+animal = 'pato'
+
+let color
+color = 'marron'
+
+const nombre = 'Agapito'
+
+console.log(`Tengo un ${animal} llamdo ${nombre} de color ${color} que tiene ${edad} años picoteandome las patas.`)
