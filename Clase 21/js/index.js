84ca7b2739e263217443daf0c60ddd34870f60f7
anddreaparra-ctrl <anddreaparra@alumnos.cei.es>
2026-09-02 11:49:40 +0200

    22

---
+++ Clase 21/js/index.js
@@ -0,0 +1,8 @@
+let nombre =  'Pepito'
+let templateString
+let edad = 23
+
+
+console.log(`Se llama ${nombre} y tiene ${edad} años.`)
+console.log(`${nombre} a los ${edad} tiene -1000 de aura actualmente.`)
+console.log(`Me llamo ${nombre} de ${edad} años. Comandante de los Ejércitos del Norte, General de las Legiones Fénix, fiel servidor del verdadero Emperador Marco Aurelio. Padre de un hijo asesinado, esposo de una esposa asesinada y juro que me vengaré, en esta vida o en la otra.`)
---
+++ Clase 22/js/index.js
@@ -0,0 +1,48 @@
+/*
+let
+var
+const
+
+if
+else
+*/ 
+
+
+
+/*
+let number = 18
+
+if (number >= 70) {
+    console.log('que viejo')
+
+} else if(number >= 18) {
+    console.log('eres mayor de edad')
+
+} else {
+    console.log('Nono')
+
+} 
+
+let edad = 18
+let scarCarnet = edad >= 18 ? true : false
+
+console.log(edad >= 18 ? 'Sí, es mayor.' : 'No, es menor.')
+console.log(scarCarnet ? 'Se puede sacar carnet' : 'No se puede sacar carnet')
+*/
+
+// &&
+
+let edad = 18
+let ticket = true
+
+if (edad >= 18 && ticket){
+    console.log('Si puede entrar.')
+} else {
+    console.log('No puede entrar.')
+}
+  
+function saludar(){
+    alert('Hola')
+
+}
+
